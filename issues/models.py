f22d59061a837d7cad349da80ac2732af5121adc
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-08-06 12:52:58 +0200

    Step 2: Design and Build  Core Models
    Category Model
    Issue Model
    Using the default django user model

--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.'
+
+# Choices for issue status
+STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('resolved', 'Resolved'),
+]
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+    
+    def __str__(self):
+        return self.name
+
+class Issue(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    location = models.CharField(max_length=255, help_text="Location details (e.g., 'Main Street, Block 4')")
+    image = models.ImageField(upload_to='issue_images/', blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    reported_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'issues'
+    
+    def __str__(self): 
+        return self.title
